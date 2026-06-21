fc05dd098f5e0c409276953bed3eba731a07470d
Ali Kutlusoy <ali@kutlusoy.com>
2026-06-21 13:49:10 +0200

    Version Update

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,7 +1,11 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
+<<<<<<< Updated upstream
   version: '4.0.1:2',
+=======
+  version: '4.0.1:3',
+>>>>>>> Stashed changes
   releaseNotes: {
     en_US: 'Initial release of Elektron Net Pool on StartOS.',
     de_DE: 'Erstveröffentlichung von Elektron Net Pool auf StartOS.',
