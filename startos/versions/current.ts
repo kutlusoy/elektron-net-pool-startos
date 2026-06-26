0942041c5d27d04fb0d7e5292e2538af6503352a
Ali Kutlusoy <ali@kutlusoy.com>
2026-06-26 12:03:31 +0200

    v update

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,10 +1,10 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '4.0.1:1',
+  version: '4.0.2:1',
   releaseNotes: {
-    en_US: 'Elektron Net Pool on StartOS.',
-    de_DE: 'Elektron Net Pool auf StartOS.',
+    en_US: 'Elektron Net Pool on StartOS, v.4.0.2:1',
+    de_DE: 'Elektron Net Pool auf StartOS, v.4.0.2:1',
   },
   migrations: {
     up: async () => {},
